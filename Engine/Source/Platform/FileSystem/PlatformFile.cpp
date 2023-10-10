423cd82a5cd52bf2c5553403f80aadcca98e0cdd
jkorn2324 <jkorn2324swagg@gmail.com>
2023-10-10 16:01:59 -0700

    fix: platform files (can now build on both macos & windows)

--- Engine/Source/Platform/FileSystem/PlatformFile.cpp
+++ Engine/Source/Platform/FileSystem/PlatformFile.cpp
@@ -13,10 +13,14 @@ bool FOpenFile(FILE** file, const wchar_t* filePath, const wchar_t* mode)
     // TODO: Implement MacOS for wide characters
     return false;
 #elif defined(PLATFORM_WINDOWS)
-    errno_t err = _wfopen_s(&filePath, fileName, mode);
+    errno_t err = _wfopen_s(file, filePath, mode);
     if (err != 0)
     {
-        fclose(*file);
+        // Guard against an empty file.
+        if (*file != nullptr)
+        {
+            fclose(*file);
+        }
         *file = nullptr;
         return false;
     }
@@ -37,7 +41,11 @@ bool FOpenFile(FILE** file, const char* filePath, const char* mode)
     errno_t err = fopen_s(file, filePath, mode);
     if (err != 0)
     {
-        fclose(*file);
+        // Guard against an empty file.
+        if (*file != nullptr)
+        {
+            fclose(*file);
+        }
         *file = nullptr;
         return false;
     }
